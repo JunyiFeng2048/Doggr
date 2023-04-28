56d6935098d63ec59f85b1a4f947c16ff18c45cc
jfeng986 <jfeng986@gmail.com>
2023-04-27 22:17:35 -0700

    migrations

--- backend/src/db/mikro-orm.config.ts
+++ backend/src/db/mikro-orm.config.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 import { TsMorphMetadataProvider } from "@mikro-orm/reflection";
+import { TSMigrationGenerator } from "@mikro-orm/migrations";
 import { defineConfig } from "@mikro-orm/postgresql";
 
 import path from "path";
@@ -18,6 +19,15 @@ const entitiesJSPath = path.join(
   "entities"
 );
 const entitiesTSPath = path.join(__dirname, "entities");
+const migrationsJSPath = path.join(
+  __dirname,
+  "..",
+  "..",
+  "build",
+  "db",
+  "migrations"
+);
+const migrationsTSPath = path.join(__dirname, "migrations");
 
 export default defineConfig({
   entities: [entitiesJSPath],
@@ -36,5 +46,31 @@ export default defineConfig({
     defaultSeeder: "DatabaseSeeder",
     glob: "!(*.d).{js,ts}", // how to match seeder files (all .js and .ts files, but not .d.ts)
     emit: "ts", // seeder generation mode
+  },
+
+  migrations: {
+    tableName: "mikro_orm_migrations", // name of database table with log of executed transactions
+    path: migrationsJSPath, // path to the folder with migrations
+    pathTs: migrationsTSPath, // path to the folder with TS migrations
+    glob: "!(*.d).{js,ts}", // how to match migration files (all .js and .ts files, but not .d.ts)
+    transactional: true, // wrap each migration in a transaction
+    disableForeignKeys: false, // wrap statements with `set foreign_key_checks = 0` or equivalent
+    allOrNothing: true, // wrap all migrations in master transaction
+    dropTables: true, // allow to disable table dropping
+    safe: false, // allow to disable table and column dropping
+    snapshot: true, // save snapshot when creating new migrations
+    emit: "ts", // migration generation mode
+    generator: TSMigrationGenerator, // migration generator, e.g. to allow custom formatting
+
+    fileName: (timestamp: string, name?: string) => {
+      // force user to provide the name, otherwise we would end up with `Migration20230421212713_undefined`
+      if (!name) {
+        throw new Error(
+          "Specify migration name via `mikro-orm migration:create --name=...`"
+        );
+      }
+
+      return `Migration${timestamp}_${name}`;
+    },
   },
 });
