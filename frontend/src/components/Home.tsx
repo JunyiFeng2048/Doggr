ad914a168d4dce2fd7830821c85101818bd8e148
jfeng986 <jfeng986@gmail.com>
2023-06-15 10:54:53 -0700

    fixed bugs

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,8 +1,21 @@
 export default function Home() {
 	return (
-		<div>
-			<h1>Doggr</h1>
-			<h3>Where your pets find love(tm)</h3>;
+		<div
+			className={
+				"flex flex-col items-center justify-center " +
+				"outline outline-offset-4 drop-shadow text-slate-50 rounded-full bg-blue-800 w-80 h-80 mx-auto mt-24"
+			}
+		>
+			<Title />
+			<Subtitle />
 		</div>
 	);
+}
+
+export function Title() {
+	return <h1 className={"text-6xl mb-4 mx-1"}>Doggr</h1>;
+}
+
+export function Subtitle() {
+	return <h3 className={"text-lg"}>Where your pets find love&trade;</h3>;
 }
